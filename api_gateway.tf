8486cb297eda879183b9a5f81bf88e05a45ff24e
ZacharyAnderson <zacha777@gmail.com>
2018-10-17 10:52:53 -0400

    Fixed terraform issues and added two necessary resources.

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,46 +1,39 @@
-resource "aws_api_gateway_rest_api" "example" {
-  name        = "ServerlessExample"
-  description = "Terraform Serverless Application Example"
+resource "aws_api_gateway_rest_api" "moonratbrains" {
+  name        = "MoonratBrains"
+  description = "Terraform Serverless Slack Bot"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  rest_api_id = "${aws_api_gateway_rest_api.moonratbrains.id}"
+  resource_id = "${aws_api_gateway_method.eventhandler.resource_id}"
+  http_method = "${aws_api_gateway_method.eventhandler.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS"
-  uri                     = "${aws_lambda_function.example.invoke_arn}"
+  uri                     = "${aws_lambda_function.lambdafunc.invoke_arn}"
 }
 
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
-  resource_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
-  http_method   = "ANY"
-  authorization = "NONE"
+resource "aws_api_gateway_method_response" "200" {
+  rest_api_id = "${aws_api_gateway_rest_api.moonratbrains.id}"
+  resource_id = "${aws_api_gateway_resource.eventhandler.id}"
+  http_method = "${aws_api_gateway_method.eventhandler.http_method}"
+  status_code = "200"
 }
 
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+resource "aws_api_gateway_integration_response" "integrationResponse" {
+  depends_on = ["aws_api_gateway_integration.lambda"]
+  rest_api_id = "${aws_api_gateway_rest_api.moonratbrains.id}"
+  resource_id = "${aws_api_gateway_resource.eventhandler.id}"
+  http_method = "${aws_api_gateway_method.eventhandler.http_method}"
+  status_code = "${aws_api_gateway_method_response.200.status_code}"
 
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.example.invoke_arn}"
 }
-
-resource "aws_api_gateway_deployment" "example" {
-  depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
-  ]
-
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
+resource "aws_api_gateway_deployment" "apigwdeployment" {
+  depends_on = ["aws_api_gateway_integration.lambda"]
+  rest_api_id = "${aws_api_gateway_rest_api.moonratbrains.id}"
   stage_name  = "test"
 }
 
-
 output "base_url" {
-  value = "${aws_api_gateway_deployment.example.invoke_url}"
+  value = "${aws_api_gateway_deployment.apigwdeployment.invoke_url}"
 }
--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 variable "bot_token" {}
 variable "coinmarketcap_token" {}
 
-resource "aws_lambda_function" "example" {
+resource "aws_lambda_function" "lambdafunc" {
   function_name = "handleMoonratEvent"
 
   # The bucket name as created earlier with "aws s3api create-bucket"
@@ -46,26 +46,26 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  parent_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
-  path_part   = "{proxy+}"
+resource "aws_api_gateway_resource" "eventhandler" {
+  rest_api_id = "${aws_api_gateway_rest_api.moonratbrains.id}"
+  parent_id   = "${aws_api_gateway_rest_api.moonratbrains.root_resource_id}"
+  path_part   = "event-handler"
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
+resource "aws_api_gateway_method" "eventhandler" {
+  rest_api_id   = "${aws_api_gateway_rest_api.moonratbrains.id}"
+  resource_id   = "${aws_api_gateway_resource.eventhandler.id}"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.example.arn}"
+  function_name = "${aws_lambda_function.lambdafunc.arn}"
   principal     = "apigateway.amazonaws.com"
 
   # The /*/* portion grants access from any method on any resource
   # within the API Gateway "REST API".
-  source_arn = "${aws_api_gateway_deployment.example.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_deployment.apigwdeployment.execution_arn}/*/*"
 }
